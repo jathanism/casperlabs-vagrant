7aa5fba1d88f83fee73e1c920430ba0ff80fde41
jathanism <jathan@gmail.com>
2020-10-11 11:16:03 -0700

    Fix the setup script for real and remove commented lines

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,30 +1,12 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
-
-# echo "Updating system packages..."
-# Update and cleanup apt first...
-# apt update
-# apt upgrade -y
-# apt autoremove --purge -y
-
-# echo "Installing build dependencies..."
-# Then install our stuff
-# apt install -y build-essential cmake
-# apt install -y protobuf-compiler
-# apt install -y python3 python3-pip python3-dev
-# apt install -y figlet
-
 # Download and install Rust without any prompts
 echo "Installing Rust..."
-# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > /tmp/install-rust.sh
-# chmod +x /tmp/install-rust.sh
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 # Install Rust as the vagrant user
 echo "Enabling Rust..."
-# sudo su - vagrant
-/tmp/install-rust.sh -y && source $HOME/.cargo/env
+source $HOME/.cargo/env
 
 # Install Casperlabs and bootstrap it
 echo "Setting up Casperlabs..."
